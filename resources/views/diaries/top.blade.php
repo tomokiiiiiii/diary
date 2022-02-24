1faf704f73a381e78b644af6c407b189bafd2a30
EC2 Default User <hiyokotomoki0810@icloud.com>
2022-02-24 20:39:07 +0900

    top (仮)

---
+++ resources/views/diaries/top.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>みんなの日記</title>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+　　</head>
+    <body>
+        <h1>日記</h1>
+        <div class='comments'>
+            <div class='comment'>
+                <p class='body'>変動する文字</p>
+            </div>
+        </div>
+    </body>
+</html>
