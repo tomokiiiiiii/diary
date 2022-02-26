e6d01793dc96f6f0615812ce2b12375727e8068d
EC2 Default User <hiyokotomoki0810@icloud.com>
2022-02-26 15:03:02 +0900

    HTML 投稿ページ

--- resources/views/diaries/top.blade.php
+++ resources/views/diaries/top.blade.php
@@ -10,7 +10,16 @@
         <div class='comments'>
             <div class='comment'>
                 <p class='body'>変動する文字</p>
+                
+                
+            <form action=>日記内容<br>
+              <textarea name="message" placeholder="今日も1日お疲れさまでした。"></textarea><br>
+               <a href="" class="btn btn--orane">戻る</a>
+              <input type="submit" value="次"/>
+              
+               <a href="" class="btn btn--orane">写真を追加　click</a>
+    　　　　　　　
+            </form>
             </div>
         </div>
     </body>
-</html>
