de95359a1191c59db18e8f98992bbe25d931f7bf
EC2 Default User <hiyokotomoki0810@icloud.com>
2022-02-27 16:06:45 +0900

    トップページ（正式）

--- resources/views/diaries/top.blade.php
+++ resources/views/diaries/top.blade.php
@@ -3,23 +3,19 @@
     <head>
         <meta charset="utf-8">
         <title>みんなの日記</title>
+        <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-　　</head>
+        <link href="{{asset('/css/top.css')}}" rel="stylesheet">
+    </head>
     <body>
         <h1>日記</h1>
-        <div class='comments'>
-            <div class='comment'>
-                <p class='body'>変動する文字</p>
+        <div class='toppages'>
+                <p class='body'>日記内容</p>
+                <p class='comment'>コメント</p>
+                <p class='emoji'>stamp</p>
                 
-                
-            <form action=>日記内容<br>
-              <textarea name="message" placeholder="今日も1日お疲れさまでした。"></textarea><br>
-               <a href="" class="btn btn--orane">戻る</a>
-              <input type="submit" value="次"/>
+                <a href="" class="btn btn--orane">次へ</a>
               
-               <a href="" class="btn btn--orane">写真を追加　click</a>
-    　　　　　　　
-            </form>
-            </div>
         </div>
     </body>
+</html>
