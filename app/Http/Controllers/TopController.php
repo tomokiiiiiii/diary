4fc405f38edf4b52a3fe681ca2d75e37a7bc3b0a
EC2 Default User <hiyokotomoki0810@icloud.com>
2022-03-01 18:59:27 +0900

    .env修正途中

--- app/Http/Controllers/TopController.php
+++ app/Http/Controllers/TopController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Top;
 use Illuminate\Http\Request;
 
 class TopController extends Controller
 {
-    //
+    public function diaries(Top $post)
+    {
+        return $post->get();
+    }
 }
